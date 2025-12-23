52b212efcf6f146a15ddebd90c53aa1fcc0dc5a3
Swift Ride User <rajaalix2022@gmail.com>
2025-12-23 12:58:04 +0500

    change in env

--- constants/Config.js
+++ constants/Config.js
@@ -1,6 +1,8 @@
-const IP_ADDRESS = process.env.EXPO_PUBLIC_SERVER_IP || "192.168.1.5"; 
-const PORT = process.env.EXPO_PUBLIC_SERVER_PORT || "8000";
+const IP_ADDRESS = process.env.EXPO_PUBLIC_SERVER_IP;
+const PORT = process.env.EXPO_PUBLIC_SERVER_PORT;
 
+console.log("IP ADDRESS:", IP_ADDRESS);
+console.log("PORT:", PORT);
 const Config = {
   API_URL: `http://${IP_ADDRESS}:${PORT}/api`,
 };
