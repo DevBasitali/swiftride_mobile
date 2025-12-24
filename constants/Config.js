dd65e70cf221451b0f43182c24983c5d9021007b
Swift Ride User <rajaalix2022@gmail.com>
2025-12-24 13:00:58 +0500

    changes

--- constants/Config.js
+++ constants/Config.js
@@ -1,8 +1,8 @@
-const IP_ADDRESS = process.env.EXPO_PUBLIC_SERVER_IP;
-const PORT = process.env.EXPO_PUBLIC_SERVER_PORT;
+// const IP_ADDRESS = process.env.EXPO_PUBLIC_SERVER_IP;
+// const PORT = process.env.EXPO_PUBLIC_SERVER_PORT;
 
-console.log("IP ADDRESS:", IP_ADDRESS);
-console.log("PORT:", PORT);
+// console.log("IP ADDRESS:", IP_ADDRESS);
+// console.log("PORT:", PORT);
 const Config = {
   API_URL: `http://${IP_ADDRESS}:${PORT}/api`,
 };
