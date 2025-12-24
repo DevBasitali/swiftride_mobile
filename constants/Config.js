d6cd4c29d3e6c661d77348793e8cc6d93d1fe1fc
Swift Ride User <rajaalix2022@gmail.com>
2025-12-24 13:07:11 +0500

    making clean config file

--- constants/Config.js
+++ constants/Config.js
@@ -1,22 +1,10 @@
-// const IP_ADDRESS = process.env.EXPO_PUBLIC_SERVER_IP;
-// const PORT = process.env.EXPO_PUBLIC_SERVER_PORT;
+const IP_ADDRESS = process.env.EXPO_PUBLIC_SERVER_IP;
+const PORT = process.env.EXPO_PUBLIC_SERVER_PORT;
 
-// console.log("IP ADDRESS:", IP_ADDRESS);
-// console.log("PORT:", PORT);
+console.log("IP ADDRESS:", IP_ADDRESS);
+console.log("PORT:", PORT);
 const Config = {
   API_URL: `http://${IP_ADDRESS}:${PORT}/api`,
 };
 
 export default Config;
-
-// // const IP_ADDRESS = '192.168.1.4';
-// const IP_ADDRESS = "192.168.1.5";
-// // const IP_ADDRESS = '127.0.0.1';
-// const PORT = "5000";
-// // const PORT = "8000";
-
-// const Config = {
-//   API_URL: `http://${IP_ADDRESS}:${PORT}/api`,
-// };
-
-// export default Config;
